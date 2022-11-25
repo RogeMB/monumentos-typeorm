dd6e46c005ee89bc6b5d1f5c91633081bf36f3a3
RogeMB <mohigefer.barog22@triana.salesianos.edu>
2022-11-25 11:44:24 +0100

    Add Swagger documentation /docs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('Monumentos API')
+  .setDescription('API de monumentos creada con NestJS, MySQL y TypeORM.')
+  .setVersion('1.0')
+  .addTag('monumentos')
+  .setContact('Rogelio Mohigefer Barrera', 'https://github.com/RogeMB/monumentos-typeorm', 'mohigefer.barog22@salesianos.triana.edu')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/monumentos/utils/types.ts
+++ src/monumentos/utils/types.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 export type CreateMonumentoParams = {
     nombre: string;
     ciudad: string;
